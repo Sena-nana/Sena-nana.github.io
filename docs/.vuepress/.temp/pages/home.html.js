7b9be029d89c3fcfe53fde6e24fad47741a5c2a0
Sena <851183156@qq.com>
2022-07-09 21:31:43 +0800

    This is a new commit

--- docs/.vuepress/.temp/pages/home.html.js
+++ docs/.vuepress/.temp/pages/home.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-79fdd481\",\"path\":\"/home.html\",\"title\":\"项目主页\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"icon\":\"home\",\"title\":\"项目主页\",\"heroImage\":\"/logo.jpg\",\"heroText\":\"魔法崩坏\",\"tagline\":\"一个泛魔法少女主题 TRPG 规则\",\"actions\":[{\"text\":\"规则书 ⭐\",\"link\":\"/guide/\",\"type\":\"primary\"},{\"text\":\"主页\",\"link\":\"/\"}],\"features\":[{\"title\":\"世界观兼容\",\"icon\":\"creative\",\"details\":\"魔法崩坏仅需少量修改就可以适应各种类魔法少女世界观\",\"link\":null},{\"title\":\"简单易懂\",\"icon\":\"support\",\"details\":\"大概吧（\",\"link\":null}],\"copyright\":\"MIT Licensed | Copyright © 2022-present 星奈 Sena\",\"footer\":\"后面没有了哦~\",\"summary\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://vuepress-theme-hope-v2-demo.mrhope.site/home.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"魔法崩坏\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"项目主页\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}]]},\"excerpt\":\"\",\"headers\":[],\"readingTime\":{\"minutes\":0,\"words\":0},\"filePathRelative\":\"home.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-79fdd481\",\"path\":\"/home.html\",\"title\":\"项目主页\",\"lang\":\"zh-CN\",\"frontmatter\":{\"home\":true,\"icon\":\"home\",\"title\":\"项目主页\",\"heroImage\":\"/logo.jpg\",\"heroText\":\"魔法崩坏\",\"tagline\":\"一个泛魔法少女主题 TRPG 规则\",\"actions\":[{\"text\":\"规则书 ⭐\",\"link\":\"/guide/\",\"type\":\"primary\"},{\"text\":\"主页\",\"link\":\"/\"}],\"features\":[{\"title\":\"世界观兼容\",\"icon\":\"creative\",\"details\":\"魔法崩坏仅需少量修改就可以适应各种类魔法少女世界观\",\"link\":null},{\"title\":\"简单易懂\",\"icon\":\"support\",\"details\":\"大概吧（\",\"link\":null}],\"copyright\":\"MIT Licensed | Copyright © 2022-present 星奈 Sena\",\"footer\":\"后面没有了哦~\",\"summary\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://vuepress-theme-hope-v2-demo.mrhope.site/home.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"魔法崩坏\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"项目主页\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2022-07-09T13:13:35.000Z\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2022-07-09T13:13:35.000Z\"}]]},\"excerpt\":\"\",\"headers\":[],\"git\":{\"createdTime\":1657372415000,\"updatedTime\":1657372415000,\"contributors\":[{\"name\":\"Sena\",\"email\":\"851183156@qq.com\",\"commits\":1}]},\"readingTime\":{\"minutes\":0,\"words\":0},\"filePathRelative\":\"home.md\",\"localizedDate\":\"2022年7月9日\"}")
